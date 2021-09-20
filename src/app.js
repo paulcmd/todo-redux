e32f26de83e6d033f933b7842220c93168457d34
Paul <choxmail.pm@gmail.com>
2021-09-19 23:50:59 -0700

    removed reducer

--- src/app.js
+++ src/app.js
@@ -1,20 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore, compose } from 'redux'
+// import { Provider } from 'react-redux'
+// import { createStore, compose } from 'redux'
 
 import IndecisionApp from './components/IndecisionApp'
-import reducer from './reducer'
+//import reducer from './reducer'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(reducer, composeEnhancers())
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+// const store = createStore(reducer, composeEnhancers())
 
-ReactDOM.render(
-    <Provider store={store}>
-        <IndecisionApp />
-    </Provider>,
-
-    document.getElementById('app')
+ReactDOM.render(<IndecisionApp />,document.getElementById('app')
 )
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,20 +1,20 @@
-import _ from 'lodash'
-const INITIAL_STATE = {
-    title: '',
-    completed: false
-}
+// import _ from 'lodash'
+// const INITIAL_STATE = {
+//     title: '',
+//     completed: false
+// }
 
-export default (state = INITIAL_STATE, action) => {
-    switch (action.type) {
-        case ADD_TODOS:
-            return {
-                ...state,
-                title: action.payload.title,
-                completed : action.payload.completed
-            }
-        case DELETE_TODOS:
-            return _.omit(state, action.payload)
-        default:
-            return state
-    }
-}
+// export default (state = INITIAL_STATE, action) => {
+//     switch (action.type) {
+//         case ADD_TODOS:
+//             return {
+//                 ...state,
+//                 title: action.payload.title,
+//                 completed : action.payload.completed
+//             }
+//         case DELETE_TODOS:
+//             return _.omit(state, action.payload)
+//         default:
+//             return state
+//     }
+// }
